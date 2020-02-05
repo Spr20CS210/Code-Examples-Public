2a47f849d8e5f94f2ddca7190ff94224f8363a3f
Tyler Conklin <tylerconklin@dhcp-10-142-222-230.uawifi.arizona.edu>
2020-02-05 15:33:42 -0700

    Update subsets example.

--- src/midterm1/E06GenSubsets.java
+++ src/midterm1/E06GenSubsets.java
@@ -11,6 +11,19 @@ public class E06GenSubsets {
      * passed in string.
      */
     public static void printSubsets(String chars) {
+        subsetsHelper(chars, "");
+    }
+
+    private static void subsetsHelper(String chars, String soFar) {
+        if (chars.isEmpty()) {
+            System.out.println(soFar);
+        } else {
+            // Include this element
+            subsetsHelper(chars.substring(1), soFar + chars.charAt(0));
+
+            // Don't include this element
+            subsetsHelper(chars.substring(1), soFar);
+        }
     }
 
 
