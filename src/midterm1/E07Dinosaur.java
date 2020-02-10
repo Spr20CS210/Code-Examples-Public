dfaac1bcb539994daeee9e347c86756e58ac51d6
Tyler Conklin <tylerconklin@dhcp-10-142-221-144.uawifi.arizona.edu>
2020-02-10 14:22:21 -0700

    Add E07 Examples

---
+++ src/midterm1/E07Dinosaur.java
@@ -0,0 +1,35 @@
+package midterm1;
+
+public class E07Dinosaur {
+
+    // This height is in meters
+    private double height;
+    private boolean isAlive;
+    private String name;
+    private String favoriteFood;
+
+    // No return type, same name as the class
+    public E07Dinosaur() {
+        height = 22.0;
+        isAlive = false;
+        name = "Brachysauraus";
+        favoriteFood = "Leaves";
+    }
+
+    public E07Dinosaur(double height, boolean isAlive, String name,
+            String favoriteFood) {
+        this.height = height;
+        this.isAlive = isAlive;
+        this.name = name;
+        this.favoriteFood = favoriteFood;
+    }
+
+    public void setHeight(double newHeight) {
+        height = newHeight;
+    }
+
+    public static void dinoRoar() {
+        System.out.println("Roar, I am dino.");
+    }
+
+}
